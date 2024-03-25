31b526cd5c0e9f3bf564367f6f4af7e8576a3144
Ana Westphalen <ana.westphalen0712@gmail.com>
2024-03-25 19:05:01 -0300

    exercise 2

--- Exercicio1.java
+++ Exercicio1.java
@@ -1,35 +1,9 @@
-//a) Crie um método chamado exercicio1a, que retorna o array de inteiros abaixo:
-//0 1 2 3 4 5 6 7 8 9
-//0 1 2 3 4 5 6 7 8 9
-//0 1 2 3 4 5 6 7 8 9
-
-//b) Crie um método chamado exercicio1b, que retorna o array de inteiros abaixo:
-//0 1 4 9 16 25 36 49 64 81
-//0 1 4 9 16 25 36 49 64 81
-//0 1 4 9 16 25 36 49 64 81
-//0 1 4 9 16 25 36 49 64 81
-//0 1 4 9 16 25 36 49 64 81
-
-//c) Crie um método chamado exercicio1c, que retorna o array de inteiros abaixo:
-//0 0 0 0 0 0
-//1 1 1 1 1 1
-//2 2 2 2 2 2
-//3 3 3 3 3 3
-//4 4 4 4 4 4
-//5 5 5 5 5 5
-
-//d) Crie um método chamado exercicio1d, que retorna o array de inteiros abaixo:
-//-1 -1 -1 -1 -1 -1
-//0 0 0 0 0 0
-//-1 -1 -1 -1 -1 -1
-//0 0 0 0 0 0
-//-1 -1 -1 -1 -1 -1
-//0 0 0 0 0 0
-//-1 -1 -1 -1 -1 -1
-//0 0 0 0 0 0
-//-1 -1 -1 -1 -1 -1
-
 public class Exercicio1 {
+    //a) Crie um método chamado exercicio1a, que retorna o array de inteiros abaixo:
+    //0 1 2 3 4 5 6 7 8 9
+    //0 1 2 3 4 5 6 7 8 9
+    //0 1 2 3 4 5 6 7 8 9
+
     public int[][] exercicio1a() {
         int[][] array = new int[3][10];
         for (int i = 0; i<array.length; i++)
@@ -38,6 +12,13 @@ public class Exercicio1 {
 
         return array;
     }
+
+    //b) Crie um método chamado exercicio1b, que retorna o array de inteiros abaixo:
+    //0 1 4 9 16 25 36 49 64 81
+    //0 1 4 9 16 25 36 49 64 81
+    //0 1 4 9 16 25 36 49 64 81
+    //0 1 4 9 16 25 36 49 64 81
+    //0 1 4 9 16 25 36 49 64 81
 
     public int[][] exercicio1b() {
         int[][] array = new int [5][10];
@@ -48,6 +29,14 @@ public class Exercicio1 {
         return array;
     }
 
+    //c) Crie um método chamado exercicio1c, que retorna o array de inteiros abaixo:
+    //0 0 0 0 0 0
+    //1 1 1 1 1 1
+    //2 2 2 2 2 2
+    //3 3 3 3 3 3
+    //4 4 4 4 4 4
+    //5 5 5 5 5 5
+
     public int[][] exercicio1c() {
         int[][] array = new int [6][6];
         for (int i = 0; i<array.length; i++)
@@ -56,6 +45,17 @@ public class Exercicio1 {
 
         return array;
     }
+
+    //d) Crie um método chamado exercicio1d, que retorna o array de inteiros abaixo:
+    //-1 -1 -1 -1 -1 -1
+    //0 0 0 0 0 0
+    //-1 -1 -1 -1 -1 -1
+    //0 0 0 0 0 0
+    //-1 -1 -1 -1 -1 -1
+    //0 0 0 0 0 0
+    //-1 -1 -1 -1 -1 -1
+    //0 0 0 0 0 0
+    //-1 -1 -1 -1 -1 -1
 
     public int[][] exercicio1d() {
         int[][] array = new int [9][6];
